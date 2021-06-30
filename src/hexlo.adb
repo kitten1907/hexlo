fea7874cd65ad99c8b67e5f6ea6afb84e348616d
kitten1907 <dswbike@gmail.com>
2021-06-30 19:54:53 -0400

    hexlo: initial build rule and source file

---
+++ src/hexlo.adb
@@ -0,0 +1,14 @@
+
+with Ada.Command_Line;
+with Ada.Exceptions;
+with Ada.Strings.Unbounded;       use Ada.Strings.Unbounded;
+with Ada.Text_IO;                 use Ada.Text_IO;
+
+procedure hexlo is
+
+begin
+
+	Put_Line("Ada hexlo program: begin");
+
+	Put_line("Ada hexlo program: end");
+end hexlo;
